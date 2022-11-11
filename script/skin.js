6d80e132669c4b21d075d7823a9c021895d8ad58
preetuuppp <preetikushwaha0110@gmail.com>
2022-11-11 23:07:17 +0530

    skinc

---
+++ script/skin.js
@@ -0,0 +1,238 @@
+let data = [
+    {
+        "title": "Faces Canada Hydro Cleanser",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/e/f/ef93c248903380157734__1_.jpg",
+        "price": 734,
+        "rating": 4.2,
+        "category": "Cleanser",
+
+    },
+    {
+        "title": "O3+ Blueberry Dtan Creme Mask",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/5/1/51e_koru3bs._sl1500_.jpg",
+        "price": 880,
+        "rating": 4.7,
+        "category": "Moisturising ",
+
+    },
+    {
+        "title": "Miss Claire Prestige Anti Dust ",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/1/a/1a89f778903487036925_1.jpg",
+        "price": 680,
+        "rating": 4.8,
+        "category": "Cleansing",
+
+    },
+    {
+        "title": " Derma Cult Retinol Cream",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/e/c/ec35271o3-0953_7_.jpg",
+        "price": 520,
+        "rating": 4.6,
+        "category": "Moisturising ",
+
+    },
+    {
+        "title": "Faces Canada Urban ",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/2/4/24706218903380002539_new_0.jpg",
+        "price": 940,
+        "rating": 4.2,
+        "category": "Cleanser",
+
+    },
+    {
+        "title": "Miss Claire Make Up Palette 9998-2 (Make Up Kit)",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-1_10_1.jpg",
+        "price": 250,
+        "rating": 4,
+        "category": "Blush",
+
+    },
+    {
+        "title": "Miss Claire Make Up Palette 9927-1 (Make Up Kit)",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/9/8903487030787.jpg",
+        "price": 490,
+        "rating": 4.8,
+        "category": "Blush",
+
+    },
+    {
+        "title": "Miss Claire Mineral Blusher Kit - 3716-4-1",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/5/b/5b60f778903487000070_1.jpg",
+        "price": 57,
+        "rating": 4.2,
+        "category": "Blush",
+
+    },
+    {
+        "title": "Miss Claire Make Up Palette 9960 (Make Up Kit)",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/9/8903487028210.jpg",
+        "price": 150,
+        "rating": 4,
+        "category": "Blush",
+
+    },
+    {
+        "title": "Miss Claire Bronze & Highlighter Makeup Studio Palette - 1",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/2/7/27f0f778903487043169_1.jpg",
+        "price": 63,
+        "rating": 4,
+        "category": "Bronzer",
+
+    },
+    {
+        "title": "Miss Claire The Cheeky Trio Bronzer Blusher Highlighter - 2",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/1/0/101_1_1.jpg",
+        "price": 280,
+        "rating": 3.9,
+        "category": "Bronzer",
+
+    },
+    {
+        "title": "Swiss Beauty Multi Purpose Palette - 01",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/s/w/sw84.jpg",
+        "price": 130,
+        "rating": 4.2,
+        "category": "Bronzer",
+
+    },
+    {
+        "title": "Colorbar Flawless Touch Contour And Highlighter - Neutral",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/1/7/171.jpg",
+        "price": 500,
+        "rating": 4.8,
+        "category": "Contour",
+
+    },
+    {
+        "title": "Blue Heaven High Coverage Foundation With Primer",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-142_1.jpg",
+        "price": 250,
+        "rating": 4,
+        "category": "Moisturising ",
+
+    },
+    {
+        "title": "Nyx Professional Makeup Highlight & Contour Pro Palette Matte Finish",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/6/86.jpg",
+        "price": 600,
+        "rating": 4.2,
+        "category": "Moisturising ",
+
+    },
+    {
+        "title": "Modern Rubber Towels",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/h/g/hgjf.jpg",
+        "price": 150,
+        "rating": 3.9,
+        "category": "Contour",
+        "id": "16"
+    },
+    {
+        "title": "Miss Claire Make Up Palette 9960 (Make Up Kit)",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/9/8903487028210.jpg",
+        "price": 150,
+        "rating": 4,
+        "category": "Contour",
+    },
+    {
+        "title": "Faces Canada Ultime Pro Blend Finity Stick Concealer",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/9/3/938ec248903380001310_1.jpg",
+        "price": 350,
+        "rating": 4.5,
+        "category": "Cleanser",
+
+    },
+    {
+        "title": "Mamaearth Glow Hydrating Concealer With Vitamin C & Turmeric For 100% Spot",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/2/d/2d81f6a8906087779186_1.jpg",
+        "price": 426,
+        "rating": 4.5,
+        "category": "Consealer",
+
+    },
+    {
+        "title": "Lakme 9to5 Primer + Matte Liquid Concealer",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/4/1/41tfhgyltzl._sl1000__1_.jpg",
+        "price": 350,
+        "rating": 4.3,
+        "category": "Cleanser",
+
+    },
+    {
+        "title": "Swiss Beauty Cover Studio Ultra Base Concealer Palette - 01",
+        "avatar": "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/2/_/2_174_4.jpg",
+        "price": 810,
+        "rating": 4.9,
+        "category": "Consealer",
+
+    },
+
+]
+function displayCard(data) {
+    document.querySelector("#container").innerHTML = ""
+    data.forEach(function (element) {
+        let div = document.createElement("div");
+        let name = document.createElement("p")
+        name.innerText = element.title;
+        let img = document.createElement("img");
+        img.src = element.avatar;
+        let price = document.createElement("p");
+        price.innerText = "price :- " + element.price + "/-";
+
+        let rate = document.createElement("p");
+        rate.innerText = "rating :- " + element.rating;
+
+        let Category = document.createElement("p");
+        Category.innerText = element.category;
+        let add = document.createElement("button")
+        add.innerText = "Add to Cart";
+        add.addEventListener("click", function () {
+            addData("cart", element)
+            alert("product added in cartData..!")
+
+
+        })
+
+        div.append(img, name, price, rate, Category, add);
+        document.querySelector("#container").append(div)
+
+
+    });
+}
+displayCard(data)
+
+
+function handle() {
+    let selected = document.querySelector("#selector").value
+    if (selected == "" || selected == "all") {
+        displayCard(data)
+    } else {
+
+        let filterData = data.filter(function (el) {
+            return el.category == selected
+        })
+        displayCard(filterData)
+    }
+
+    // console.log(filterData)
+}
+
+function addData(key, value) {
+    let newData = JSON.parse(localStorage.getItem(key)) || [];
+    newData.push(value);
+
+    localStorage.setItem(key, JSON.stringify(newData))
+}
+
+function ppp() {
+    let p = document.querySelector("input").value
+    let newData = data.filter(function (ele) {
+        return ele.title.toLowerCase().includes(p.toLocaleLowerCase());
+
+
+    });
+    displayCard(newData)
+
+}
+
+
